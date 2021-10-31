4d181a3c1453ce9b3727b2545c2e19df21ffc8e8
Loxi <dianyuyi@gmail.com>
2021-10-31 22:41:49 +0800

    Api route test

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -7,7 +7,7 @@ const ArticleItem = ({article}) => {
     <Link href="/article/[id]" as={`/article/${article.id}`}>
       <a className={articleStyles.card}>
         <h3>{article.title}</h3>
-        <p>{article.body}</p>
+        <p>{article.excerpt}</p>
       </a>
     </Link>
   )
